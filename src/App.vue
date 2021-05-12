25495775bdbcafcbebc2ae2e60ee0dc520cb577d
deveyloper <deveyloper@gmail.com>
2021-05-12 14:18:46 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,59 @@
 <template>
   <div class="container">
-    <div class="logo">logo</div>
+    <div class="logo">Notes</div>
     <div class="header">header</div>
-    <div class="menu">menu</div>
-    <div class="content">content</div>
-    <div class="subcontent">sub content</div>
+    <div class="menu">
+      <p>Menu 1</p>
+      <p>Menu 2</p>
+      <p>Menu 3</p>
+      <p>Menu 4</p>
+      <p>Menu 5</p>
+    </div>
+    <div class="content">
+      <Note v-for="note in notes" :note="note" :key="note.title" />
+    </div>
+    <div class="subcontent">{{ selectedNote }}</div>
     <div class="footer">footer</div>
   </div>
 </template>
 
 <script>
+import Note from "./components/Note.vue";
 export default {
   name: "App",
+  components: { Note },
+  data() {
+    return {
+      selectedNote: "abc",
+      notes: [
+        {
+          title: "a",
+          body: "1",
+        },
+        {
+          title: "b",
+          body: "2",
+        },
+        {
+          title: "c",
+          body: "3",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
 body * {
   font-family: sans-serif;
-  text-align: center;
   color: black;
 }
 
 .container {
   display: grid;
   grid-template: repeat(10, 1fr) / repeat(10, 1fr);
-  height: 90vh;
+  height: 100vh;
 }
 
 .header {
@@ -38,11 +66,15 @@ body * {
   grid-column: 1/3;
   grid-row: 1/3;
   background-color: chocolate;
+  font-size: 2em;
+  font-weight: bold;
+  padding: 20px;
 }
 .menu {
   background-color: red;
   grid-column: 1/3;
   grid-row: 3/10;
+  padding: 20px;
 }
 
 .footer {
@@ -55,11 +87,15 @@ body * {
   grid-column: 3/7;
   grid-row: 3/10;
   border-top-left-radius: 10%;
+  padding: 20px;
+  display: grid;
+  grid-gap: 5px;
 }
 .subcontent {
   background-color: darkmagenta;
   grid-column: 7/11;
   grid-row: 3/10;
+  border-top-right-radius: 10%;
 }
 
 .footer {
---
+++ src/components/Note.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="note">
+    {{ note.body }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Note",
+  props: ["note"],
+  mounted() {
+    console.log(this.note);
+  },
+};
+</script>
+
+<style scoped>
+.note {
+  height: 75px;
+  background-color: #dddddd;
+  padding: 5px;
+}
+</style>
